f2464cd15f3639964b9bbf51a37b64832c26d963
NNADI HENRY UGONNA <greyios845@gmail.com>
2023-09-23 18:59:16 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Grey  on 23.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
